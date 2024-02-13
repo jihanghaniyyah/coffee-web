0bfe18d08027e5e43b85ee50d26d2c60c311568b
jihanghaniyyah <jghaniyyah@gmail.com>
2024-02-13 12:06:23 +0700

    add: new sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Menu from './components/Menu';
 
 const App = () => {
 	return (
 		<div>
 			<Navbar />
+			<main>
+				<div id='home'>
+					<Home />
+				</div>
+				<div id='menu'>
+					<Menu />
+				</div>
+			</main>
 		</div>
 	);
 };
