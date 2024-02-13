c2613e4ece4848af3eaec6247d5a992c47777990
jihanghaniyyah <jghaniyyah@gmail.com>
2024-02-13 21:03:58 +0700

    fix: add the other section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Menu from './components/Menu';
+import About from './components/About';
+import Product from './components/Product';
+import Review from './components/Review';
+import Footer from './components/Footer';
 
 const App = () => {
 	return (
@@ -14,7 +18,17 @@ const App = () => {
 				<div id='menu'>
 					<Menu />
 				</div>
+				<div id='about'>
+					<About />
+				</div>
+				<div id='products'>
+					<Product />
+				</div>
+				<div id='review'>
+					<Review />
+				</div>
 			</main>
+			<Footer />
 		</div>
 	);
 };
