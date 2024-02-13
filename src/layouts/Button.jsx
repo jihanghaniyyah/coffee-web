b484ed58b7b0b92c7f5632059e47f997cc39b628
jihanghaniyyah <jghaniyyah@gmail.com>
2024-02-13 12:04:15 +0700

    style: button

--- src/layouts/Button.jsx
+++ src/layouts/Button.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Button = (props) => {
 	return (
-		<div className='px-6 py-1 border-white bg-[#FFDCAB] hover:text-[#AB6B2E] transition-all rounded-full'>
-			<button>{props.title}</button>
+		<div>
+			<button className='px-6 py-1 border-2 border-white bg-[#FFDCAB] hover:text-[#AB6B2E] transition-all rounded-full'>
+				{props.title}
+			</button>
 		</div>
 	);
 };
